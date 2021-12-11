870f0fd2964dc98027bbfce0ea548a87840a717b
Benjamin <benjamin_gaertner@gmx.net>
2021-12-11 08:08:28 +0100

    Fixed mistakes

--- 07/pizzle-07-2.py
+++ 07/pizzle-07-2.py
@@ -1,4 +1,4 @@
-minimum_fuel_cost = 1e6
+minimum_fuel_cost = 1e22
 
 with open("07/input-07.txt", "r") as f:
     data = f.read().strip().split(",")
@@ -8,7 +8,8 @@ data = [int(value) for value in data]
 for position in range(min(data), max(data) + 1):
     fuel_cost = 0
     for value in data:
-        fuel_cost += abs(position - value)
+        cost = abs(position - value)
+        fuel_cost += int((cost * (cost + 1)) / 2)
     if fuel_cost < minimum_fuel_cost:
         minimum_fuel_cost = fuel_cost
 
